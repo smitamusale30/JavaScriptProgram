be472387c1648f7192ef612a1334d4194d64549e
smitamusale30 <smitamusale30@gmail.com>
2021-01-06 20:20:39 +0530

    updated: file

--- concatenation.js
+++ concatenation.js
@@ -4,4 +4,4 @@ const age=22;
 console.log("My name is :  "+name+" and age is "+age);
 //template String
 
-console.log('my name is ${name} and i am ${age}');
+console.log(`my name is ${name} and i am ${age}`);
