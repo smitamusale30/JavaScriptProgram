157528e8e27f83484cf4d115894360017030a84e
smitamusale30 <smitamusale30@gmail.com>
2021-01-07 12:25:50 +0530

    Updated; new function

--- functionProgram.js
+++ functionProgram.js
@@ -1,6 +1,27 @@
+//normal Function
 function addition(num1,num2) {
 
     console.log('Addition is: '+(num1+num2));
     
 }
-addition(5,4);
+addition(5,4)
+
+//function with return statement
+function addition1(num1,num2) {
+
+    return num1+num2;
+    
+}
+console.log(addition1(5,4));
+
+//Arrow function introduse ES6 or ES2015
+const addition2 =(num1=1,num2=1)=> {
+
+    console.log('Addition is: '+(num1+num2));
+    
+}
+addition2(5,4)
+
+//Arrow function 2
+const addition3 =(num1=1,num2=1)=>  (num1+num2);
+console.log(addition3(5,4));
