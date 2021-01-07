a6d458f565abea5dccd6e6930a89ffb590d900f2
smitamusale30 <smitamusale30@gmail.com>
2021-01-07 12:01:09 +0530

    add ; if statement

---
+++ if..Statements.js
@@ -0,0 +1,28 @@
+const x=10;
+if(x==10)
+{
+    console.log('x is 10');
+}
+
+if(x==20)
+{
+    console.log('x is 20');
+}
+else
+{
+    console.log('x is not 20');
+}
+
+const y=-3;
+if(y < 0)
+{
+    console.log('y is negative');
+}
+else if(y > 0)
+{
+    console.log('y is positive');
+}
+else 
+{
+    console.log('y is zero');
+}
